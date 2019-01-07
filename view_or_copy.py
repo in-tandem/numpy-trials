c488829914119f3b5dd6b1da60265d823a3346d9
somak <somaktukai@gmail.com>
2019-01-07 21:24:18 +0530

    exaplanation of [[1,1]] selction..

--- view_or_copy.py
+++ view_or_copy.py
@@ -7,7 +7,7 @@ def execute():
     ##how to check if an array shares the same memory (ie a block)
     ## you call.base.. if you .base is NOne it is a copy else a view
     b = a[1:,:1]
-    c = a[[1,1]] #fancy indexing..returns a copy
+    c = a[[1,1]] #fancy indexing..returns a copy..row1 returned twice
     print(a,b, b.base is a, b.base)
     print(a,c, c.base is a, c.base)
 
